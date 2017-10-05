456e9a036fe257e86bf5b6e1c7418efca87d9892
GoldTwo <goldtwo1298@gmail.com>
2017-10-05 09:07:35 -0600

    Worked on scrollmagic

--- JS/main.js
+++ JS/main.js
@@ -2,7 +2,12 @@ $(document).ready(function(){
 	var controller = new ScrollMagic.Controller();
 	
     var wipetimeline = new TimelineMax()
-        .fromTo("#item1", 1, {y: "100%"}, {y: "0%", ease: Linear.easeNone});
+        .fromTo("#item1", 1, {y: "100%"}, {y: "0%", ease: Linear.easeNone})
+        .fromTo("#transition1", 1, {opacity: "0%"}, {opacity: "100%", ease: Linear.easeNone})
+        .fromTo("#item2", 1, {y: "100%"}, {y: "0%", ease: Linear.easeNone})
+        .fromTo("#transition2", 1, {opacity: "0%"}, {opacity: "100%", ease: Linear.easeNone})
+        .fromTo("#item3", 1, {y: "100%"}, {y: "0%", ease: Linear.easeNone})
+        .fromTo("#transition3", 1, {opacity: "0%"}, {opacity: "100%", ease: Linear.easeNone});
     
 	var ourScene = new ScrollMagic.Scene({
 		triggerElement: "#bodyitemcontainer",
