94adf1a4e3e6d9feeac53b340e97c1d47d8c7741
Gold Two <goldtwo1298@gmail.com>
2017-10-06 17:59:29 -0600

    Scrollmagic is sucking my soul from my body no matter what I do NOTHING will work, send help before scrollmagic consumes me

--- JS/main.js
+++ JS/main.js
@@ -1,21 +1,21 @@
-$(document).ready(function(){
+$(function () {
 	var controller = new ScrollMagic.Controller();
 	
-    var wipetimeline = new TimelineMax()
-        .fromTo("#item1", 1, {y: "100%"}, {y: "0%", ease: Linear.easeNone})
-        .fromTo("#transition1", 1, {opacity: "0%"}, {opacity: "100%", ease: Linear.easeNone})
-        .fromTo("#item2", 1, {y: "100%"}, {y: "0%", ease: Linear.easeNone})
-        .fromTo("#transition2", 1, {opacity: "0%"}, {opacity: "100%", ease: Linear.easeNone})
-        .fromTo("#item3", 1, {y: "100%"}, {y: "0%", ease: Linear.easeNone})
-        .fromTo("#transition3", 1, {opacity: "0%"}, {opacity: "100%", ease: Linear.easeNone});
+    var wipeAnimation = new TimelineMax()
+//        .fromTo("#item1", 1, {y: "100%"}, {y: "0%", ease: Linear.easeNone})
+//        .fromTo("#transition1", 1, {opacity: "0%"}, {opacity: "100%", ease: Linear.easeNone})
+        .fromTo("section.bodyitem.item2", 1, {y: "-100%"}, {y: "0%", ease: Linear.easeNone})
+//        .fromTo("#transition2", 1, {opacity: "0%"}, {opacity: "100%", ease: Linear.easeNone})
+        .fromTo("section.bodyitem.item3", 1, {y: "-100%"}, {y: "0%", ease: Linear.easeNone});
+//        .fromTo("#transition3", 1, {opacity: "0%"}, {opacity: "100%", ease: Linear.easeNone});
     
-	var scene = new ScrollMagic.Scene({
+	new ScrollMagic.Scene({
 		triggerElement: "#bodyitemcontainer",
-        triggerHook: "0",
-        duration: "600%"
+        triggerHook: "onLeave",
+        duration: "200%"
 	})
 	.setPin("#bodyitemcontainer")
-    .setTween(wipetimeline)
+    .setTween(wipeAnimation)
     .addIndicators()
     .addTo(controller)
 });
