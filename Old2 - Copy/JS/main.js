63628a6ec380ccb35d2c52f5cd4940d2abe02526
GoldTwo <goldtwo1298@gmail.com>
2017-10-09 11:45:37 -0600

    Now I have a choice of two styles one still half broken however and theres still a ton ofwork that needs to be done

--- Old2 - Copy/JS/main.js
+++ Old2 - Copy/JS/main.js
@@ -2,14 +2,17 @@ $(function () {
 	var controller = new ScrollMagic.Controller();
 	
     var wipeAnimation = new TimelineMax()
-        .fromTo("div.bodyitem.wipeoverlay1", 1, {opacity: 0}, {opacity: 1, ease: Linear.easeNone})
-        .fromTo("div.bodyitem.wipeoverlay1", 1, {opacity: 1}, {opacity: 0, ease: Linear.easeNone})
+//        .fromTo("div.bodyitem.wipeoverlay1", 1, {opacity: 0}, {opacity: 1, ease: Linear.easeNone})
+//        .fromTo("div.bodyitem.wipeoverlay1", 1, {opacity: 1}, {opacity: 0, ease: Linear.easeNone})
+        .to("div.bodyitem.wipeoverlay1", 1, {opacity: 1})
     
-        .fromTo("div.bodyitem.wipeoverlay2", 1, {opacity: 0}, {opacity: 1, ease: Linear.easeNone})
-        .fromTo("div.bodyitem.wipeoverlay2", 1, {opacity: 1}, {opacity: 0, ease: Linear.easeNone});
+//        .fromTo("div.bodyitem.wipeoverlay2", 1, {opacity: 0}, {opacity: 1, ease: Linear.easeNone})
+//        .fromTo("div.bodyitem.wipeoverlay2", 1, {opacity: 1}, {opacity: 0, ease: Linear.easeNone});
+        .to("div.bodyitem.wipeoverlay2", 1, {opacity: 1})
     
 //        .fromTo("div.bodyitem.wipeoverlay3", 1, {opacity: 0}, {opacity: 1, ease: Linear.easeNone})
 //        .fromTo("div.bodyitem.wipeoverlay3", 1, {opacity: 1}, {opacity: 0, ease: Linear.easeNone});
+        .to("div.bodyitem.wipeoverlay3", 1, {opacity: 1});
     
     
 	new ScrollMagic.Scene({
